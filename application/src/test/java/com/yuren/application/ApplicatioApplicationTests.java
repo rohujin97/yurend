b5d12cb7588b0ea0cb122a14d9762bb1c8e8232a
rohujin97 <rohujin98@naver.com>
2026-02-01 21:53:20 +0900

    initial commit

---
+++ application/src/test/java/com/yuren/application/ApplicatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuren.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicatioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
